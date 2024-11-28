fd4250ad0a3e884718750b7bade86255681b0267
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-11-28 14:32:37 +0100

    add signout btn

--- src/app/posts.tsx
+++ src/app/posts.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { H2 } from "../components/common/H2";
 
 export default function Posts() {
   return (
-    <View>
-      <Text>Posts</Text>
+    <View className="flex-1 dark:bg-black">
+      <H2>Posts</H2>
     </View>
   );
 }
--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -1,18 +1,23 @@
-import { useUser } from "@clerk/clerk-expo";
+import { useClerk, useUser } from "@clerk/clerk-expo";
 import SignInScreen from "./(auth)/signin";
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import { H2 } from "../components/common/H2";
 
 export default function Profile() {
   const { user } = useUser();
+  const { signOut } = useClerk();
 
   if (!user) {
     return <SignInScreen />;
   }
 
   return (
-    <View className="flex-1 items-center justify-center dark:bg-black">
-      <H2 className="dark:text">Profile</H2>
+    <View className="flex-1 flex gap-5 dark:bg-black">
+      <H2>Add Post</H2>
+      <H2>My Posts</H2>
+      <TouchableOpacity onPress={() => signOut()}>
+        <H2>Sign Out</H2>
+      </TouchableOpacity>
     </View>
   );
 }
