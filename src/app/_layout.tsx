ef4eba951974557fb495c4873e406c4ac211377c
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-11-28 09:19:34 +0100

    add theme provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,33 +2,19 @@ import { Stack } from "expo-router";
 import "../../global.css";
 import { StatusBar, TouchableOpacity, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
-import { useColorScheme } from "nativewind";
+import { ThemeProvider, useTheme } from "../lib/providers/ThemeProvider";
 
 export default function RootLayout() {
-  const { colorScheme, toggleColorScheme } = useColorScheme();
-
   return (
-    <>
-      <StatusBar backgroundColor={colorScheme === "dark" ? "#000" : "#fff"} />
+    <ThemeProvider>
       <Stack>
         <Stack.Screen
           name="index"
           options={{
             headerTitle: "Index",
-            headerRight: () => (
-              <View style={{ marginRight: 10 }}>
-                <TouchableOpacity onPress={toggleColorScheme}>
-                  <Feather
-                    name={colorScheme === "dark" ? "sun" : "moon"}
-                    size={24}
-                    color={"#000"}
-                  />
-                </TouchableOpacity>
-              </View>
-            ),
           }}
         />
       </Stack>
-    </>
+    </ThemeProvider>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,17 +2,34 @@ import { MyView } from "../components/common/MyView";
 import { MyText } from "../components/common/MyText";
 import { H1 } from "../components/common/H1";
 import { H2 } from "../components/common/H2";
-import { StatusBar } from "react-native";
+import { StatusBar, TouchableOpacity, View } from "react-native";
+import { useTheme } from "../lib/providers/ThemeProvider";
+import { Feather } from "@expo/vector-icons";
 
 export default function Index() {
+  const { colorScheme, toggleColorScheme } = useTheme();
+
   return (
     <>
-      <MyView className="justify-center items-center dark:bg-black">
+      <StatusBar
+        backgroundColor={colorScheme === "dark" ? "#000" : "#fff"}
+        barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+      />
+      <MyView className="justify-center flex-1 items-center dark:bg-black">
         <MyText className="text-green-600">
           Edit app/index.tsx to edit this screen.ss
         </MyText>
         <H1>hello</H1>
         <H2>world</H2>
+        <View style={{ marginRight: 10 }}>
+          <TouchableOpacity onPress={toggleColorScheme}>
+            <Feather
+              name={colorScheme === "dark" ? "sun" : "moon"}
+              size={24}
+              color={colorScheme === "dark" ? "#fff" : "#000"}
+            />
+          </TouchableOpacity>
+        </View>
       </MyView>
     </>
   );
